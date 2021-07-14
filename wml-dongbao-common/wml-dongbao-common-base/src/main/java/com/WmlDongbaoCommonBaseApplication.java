7621669d73ce9d0d647498fcfdcb9070aab3ab5b
1910978246 <85920538+1910978246@users.noreply.github.com>
2021-07-14 13:50:57 +0800

    给common-base添加启动类、测试类、

---
+++ wml-dongbao-common/wml-dongbao-common-base/src/main/java/com/WmlDongbaoCommonBaseApplication.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * @Auther: 王明礼
+ * @Date: 2021/7/14 - 07 - 14 - 13:45
+ * @Description: com
+ * @version: 1.0
+ */
+public class WmlDongbaoCommonBaseApplication {
+}
---
+++ wml-dongbao-common/wml-dongbao-common-base/src/test/java/com/wml/wmldongbaocommonbase/WmlDongbaoCommonBaseApplicationTests.java
@@ -0,0 +1,10 @@
+package com.wml.wmldongbaocommonbase;
+
+/**
+ * @Auther: 王明礼
+ * @Date: 2021/7/14 - 07 - 14 - 13:47
+ * @Description: com.wml.wmldongbaocommonbase
+ * @version: 1.0
+ */
+public class WmlDongbaoCommonBaseApplicationTests {
+}
