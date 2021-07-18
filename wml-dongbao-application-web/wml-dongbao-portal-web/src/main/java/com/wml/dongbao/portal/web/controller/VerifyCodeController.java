073fae215db6dfdf9d995a894b14d32fa20ffb31
1910978246 <85920538+1910978246@users.noreply.github.com>
2021-07-18 21:28:26 +0800

    将图片用base64编码成字符串

--- wml-dongbao-application-web/wml-dongbao-portal-web/src/main/java/com/wml/dongbao/portal/web/controller/VerifyCodeController.java
+++ wml-dongbao-application-web/wml-dongbao-portal-web/src/main/java/com/wml/dongbao/portal/web/controller/VerifyCodeController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
 
 /**
  * @Auther: 王明礼
@@ -22,9 +23,10 @@ import java.io.ByteArrayInputStream;
 public class VerifyCodeController {
     String attrName = "verifyCode";
 
-    @GetMapping("/generator")//生成验证码接口
+    @GetMapping("/generator-base64")//生成验证码接口
     @TokenCheck(required = false)
-    public void generatorCode(HttpServletRequest request , HttpServletResponse response){
+    public String generatorCodeBase64(HttpServletRequest request, HttpServletResponse response)
+    {
 
         try {
 
@@ -33,27 +35,51 @@ public class VerifyCodeController {
             //得到验证码的值
             String code = imageCode.getCode();
 
-            request.getSession().setAttribute(attrName,code);
+            request.getSession().setAttribute(attrName, code);
 
             //得到验证码的图片
             ByteArrayInputStream image = imageCode.getImage();
 
-            //写出去
-            response.setContentType("image/jpeg");
+            //把图片上的数字存到session中
+            request.getSession().setAttribute(attrName, code);
+
+            //把图片转变成base64  把二进制字节ByteArrayInputStream转成字节数组
+            ByteArrayOutputStream swapStream = new ByteArrayOutputStream();
+            byte[] buff = new byte[1024];
+            int r = 0;
+            while ((r = image.read(buff, 0, 1024)) > 0)
+            {
+
+                swapStream.write(buff, 0, r);
+
+            }
+            byte[] data = swapStream.toByteArray();
+
+            return Base64.getEncoder().encodeToString((data));
+
+
+            //写出去到浏览器上
+            /*response.setContentType("image/jpeg");
             byte[] bytes = new byte[1024];
 
             try(ServletOutputStream out = response.getOutputStream()) {
                 while (image.read(bytes)!=-1){
                     out.write(bytes);
                 }
-            }
-        }catch (Exception e){
+            }*/
+
+
+        } catch (Exception e)
+        {
 
             System.out.println("异常");
+            return "";
 
         }
 
     }
+
+
 
     @GetMapping("/verify")//校验验证码接口
     @TokenCheck(required = false)
