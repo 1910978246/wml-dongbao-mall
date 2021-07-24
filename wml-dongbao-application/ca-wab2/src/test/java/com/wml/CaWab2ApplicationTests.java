d7f62993221eb2cc5b19eb6202eeeefb61a42afe
1910978246 <85920538+1910978246@users.noreply.github.com>
2021-07-24 12:52:34 +0800

    nginx

---
+++ wml-dongbao-application/ca-wab2/src/test/java/com/wml/CaWab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaWab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
